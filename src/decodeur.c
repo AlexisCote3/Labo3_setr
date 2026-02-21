6b8f621f5a1cce4289dd3bd8424f0195cc46aaec
Pierre-Olivier Thibault <pothi5@ulaval.ca>
2026-02-21 17:40:04 -0500

    first version decodeur

--- src/decodeur.c
+++ src/decodeur.c
@@ -10,6 +10,8 @@
 
 // Gestion des ressources et permissions
 #include <sys/resource.h>
+#include <unistd.h>
+#include <getopt.h>
 
 #include "allocateurMemoire.h"
 #include "commMemoirePartagee.h"
@@ -20,13 +22,17 @@
 // Définition de diverses structures pouvant vous être utiles pour la lecture d'un fichier ULV
 #define HEADER_SIZE 4
 const char header[] = "SETR";
+#define ULV_HEADER_BYTES 20  // 4 + 4*4
 
-struct videoInfos{
-        uint32_t largeur;
-        uint32_t hauteur;
-        uint32_t canaux;
-        uint32_t fps;
-};
+
+
+//Existe deja dans commMemoirePartagee
+// struct videoInfos{
+//         uint32_t largeur;
+//         uint32_t hauteur;
+//         uint32_t canaux;
+//         uint32_t fps;
+// };
 
 /******************************************************************************
 * FORMAT DU FICHIER VIDEO
@@ -44,6 +50,25 @@ struct videoInfos{
 * ...                             Toutes les images composant la vidéo, à la suite
 *            4          uint32    0 (indique la fin du fichier)
 ******************************************************************************/
+
+static int read_u32(const uint8_t* buffer, size_t buffer_size, size_t offset, uint32_t* out) {
+
+    if (offset + 4 > buffer_size) return -1;
+    uint32_t value;
+    memcpy(&v, buffer + offset, 4);
+    *out = value;
+    return 0;
+
+}
+
+static uint64_t now_us(void) {
+
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    // (secondes -> us) + (ns -> us)
+    return (uint64_t)ts.tv_sec * 1000000ULL + (uint64_t)ts.tv_nsec / 1000ULL;
+}
+
 
 
 int main(int argc, char* argv[]){
@@ -64,6 +89,185 @@ int main(int argc, char* argv[]){
     // N'oubliez pas que vous pouvez utiliser jpgd::decompress_jpeg_image_from_memory()
     // pour décoder une image JPEG contenue dans un buffer!
     // N'oubliez pas également que ce décodeur doit lire les fichiers ULV EN BOUCLE
+
+    struct SchedParams params;
+    memset(&params, 0, sizeof(params));
+    params.modeOrdonnanceur = ORDONNANCEMENT_NORT; //par defaut
+
+    const char* fichier_entree = NULL;
+    const char* flux_sortie = NULL;
+
+    if (argc == 2 && strcmp(argv[1], "--debug") == 0) {
+
+        //todo modifier aux bons paths
+        fichier_entree = "video.ulv";
+        flux_sortie = "/flux1_mem";
+    } else {
+
+        int opt;
+        while ((opt = getopt(argc, argv, "s:d:")) != -1) {
+
+            switch (opt) {
+
+                case 's':
+                    parseSchedOption(optarg, &params);
+                    break;
+
+                case 'd':
+                    parseDeadlineParams(optarg, &params);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        if (optind + 2 > argc) {
+            fprintf(stderr, "Usage: %s [options] fichier_entree flux_sortie\n", argv[0]);
+            fprintf(stderr, "  options: -s {NORT|RR|FIFO|DEADLINE}  -d runtime,deadline,period (ms)\n");
+            return 1;
+        }
+
+        fichier_entree = argv[optind];
+        flux_sortie    = argv[optind + 1];
+    }
+
+    if (appliquerOrdonnancement(&params, nomProgramme) != 0) {
+            fprintf(stderr, "[%s] appliquerOrdonnancement() a échoué: %s\n", nomProgramme, strerror(errno));
+            return 1;
+        }
+
+        int fd = open(fichier_entree, O_RDONLY);
+        if (fd < 0) {perror("open"); return -1;}
+
+        struct stat st;
+        if (fstat(fd, &st) != 0) {
+
+            perror("fstat");
+            close(fd);
+            return 1;
+        }
+
+        size_t file_size = (size_t)st.st_size;
+
+        uint8_t* file = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE | MAP_POPULATE, fd, 0);
+        close(fd);
+        if (file == MAP_FAILED) {
+
+            perror("mmap");
+            return 1;
+        }
+
+        uint32_t width=0,height=0,channels=0,fps=0;
+        if (read_u32(file, file_size, 4, &width) != 0 ||
+            read_u32(file, file_size, 8, &height) != 0 ||
+            read_u32(file, file_size, 12, &channels) != 0 ||
+            read_u32(file, file_size, 16, &fps) != 0
+        ) {
+            fprintf(stderr, "ULV invalide (infos)\n");
+            munmap(file, file_size);
+            return 1;
+        }
+
+        size_t frame_size = (size_t)width * (size_t)height * (size_t)channels;
+        if (frame_size == 0) {
+            fprintf(stderr, "ULV invalide (frame_size=0)\n");
+            munmap(file, file_size);
+            return 1;
+        }
+
+
+
+        //----
+        //Init memoire partager
+        //----
+
+        struct memPartage zoneOut;
+        struct videoInfos infos;
+        infos.largeur = width;
+        infos.hauteur = height;
+        infos.canaux  = channels;
+        infos.fps     = fps;
+
+        if (initMemoirePartageeEcrivain(flux_sortie, &zoneOut, &infos) != 0) {
+            perror("initMemoirePartageeEcrivain");
+            munmap(file, file_size);
+            return 1;
+        }
+
+        //periode selon le fps qui est set
+        const uint64_t frame_period_us = (fps == 0) ? 0 : (1000000UL/ (uint64_t)fps);
+
+        //----
+        //Boucle ULV
+        //----
+        size_t offset = ULV_HEADER_BYTES;
+
+        while (1) {
+
+            uint64_t t0 = now_us();
+
+            uint32_t jpegSize = 0;
+
+            if (read_u32(file, file_size, offset, &jpegSize) != 0) {
+                offset = ULV_HEADER_BYTES;
+                continue;
+            }
+
+            offset += 4;
+
+            if (jpegSize == 0) {
+                // fin recommencer au debut
+                offset = ULV_HEADER_BYTES;
+                continue;
+            }
+
+            if (offset + (size_t)jpegSize > file_size) {
+                //corruption on recommence
+                offset = ULV_HEADER_BYTES;
+                continue;
+            }
+
+            //pointeur sur les donnee du frame jpeg
+            const unsigned char* jpegBuffer = (const unsigned char*)(file + offset);
+            //avancer offset au prochain frame
+            offset += (size_t)jpegSize;
+
+            evenementProfilage(&profInfos, ETAT_TRAITEMENT);
+
+            //decompresser le jpeg en image
+            int out_width = 0, out_height = 0, out_color = 0;
+            unsigned char* decoded = decompress_jpeg_image_from_memory(jpegBuffer, (int)jpegSize, &out_width, &out_height, &out_color, (int)c, 0);
+
+            if (!decoded) {
+                continue;
+            }
+
+            evenementProfilage(&profInfos, ETAT_ATTENTE_MUTEXECRITURE);
+
+            //ecrivain prend le mutex
+            if (attenteEcrivain(&zoneOut) != 0) {
+                free(decoded);
+                continue;
+            }
+
+            memcpy(zoneOut.data, decoded, frame_size);
+            //signaler terminer decrire.
+            signalEcrivain(&zoneOut);
+
+            free(decoded);
+
+            if (frame_period_us > 0) {
+                uint64_t t1 = now_us();
+                uint64_t elapsed = t1 - t0;
+                if (elapsed < frame_period_us) {
+                    evenementProfilage(&profInfos, ETAT_ENPAUSE);
+                    usleep((useconds_t)(frame_period_us - elapsed));
+                }
+            }
+
+        }
+
 
     return 0;
 }
